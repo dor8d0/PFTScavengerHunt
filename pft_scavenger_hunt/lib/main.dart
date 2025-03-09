747b281a902b3b9b28b000bf0ef5292dc6c534a6
dor8d0 <troyw792@gmail.com>
2025-03-08 22:11:51 -0600

    Adds ability to go to different pages

--- pft_scavenger_hunt/lib/main.dart
+++ pft_scavenger_hunt/lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+//array for holding answers
+List<String> answers = ["N","N","N","N","N"];
+
 void main() {
   runApp(const MaterialApp(home: HomeScreen()));
 }
@@ -26,13 +29,14 @@ class _HomeScreenState extends State<HomeScreen> {
             SizedBox(width: 300, height: 200, child: Image.asset("images\\LSU_Athletics_logo.svg.png", fit: BoxFit.scaleDown,)),
             SizedBox(width: 300, height: 200, child: Image.asset("images\\lsu-patrick-taylor-hall-exterior-architecture.jpg", fit: BoxFit.scaleDown,)),
             SizedBox(width: 300, height: 200, child: 
-              ListView(children: [ElevatedButton(onPressed: (){}, child: Text("Q1")), 
-                Text("Q2"),
+              ListView(children: [ 
+                Text("Choose your question", selectionColor: Color.fromARGB(255, 255, 255, 255),),
+                ElevatedButton(onPressed: (){Navigator.push(context, MaterialPageRoute(builder: (context) => Q1Screen()));}, child: Text("Q1")),
                 ElevatedButton(onPressed: (){}, child: Text("Q1")),
                 ElevatedButton(onPressed: (){}, child: Text("Q1")),
                 ElevatedButton(onPressed: (){}, child: Text("Q1")),
                 ElevatedButton(onPressed: (){}, child: Text("Q1")),
-                ElevatedButton(onPressed: (){}, child: Text("Q1")),],
+                ],
               )
             )
           ],
@@ -43,4 +47,20 @@ class _HomeScreenState extends State<HomeScreen> {
 }
 
 //First question screen
-
+class Q1Screen extends StatelessWidget{
+  
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 95, 8, 62),
+      body: Center(
+        child: Column(
+          children: [
+            Text("Q1 Screen"),
+            ElevatedButton(onPressed: (){answers[0] = "A"; Navigator.pop(context);}, child: Text("Answer 1"))
+          ],
+        ),
+      ),
+    );
+  }
+}
