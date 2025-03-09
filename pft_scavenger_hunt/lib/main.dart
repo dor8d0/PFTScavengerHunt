304a741c29add1bdd83eddb1a122798d199baac1
dor8d0 <troyw792@gmail.com>
2025-03-08 22:23:16 -0600

    Adds screen for all five questions

--- pft_scavenger_hunt/lib/main.dart
+++ pft_scavenger_hunt/lib/main.dart
@@ -17,7 +17,6 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   String? selectedFood;
   int? dogAge;
-  List<String>? answers;
 
   @override
   Widget build(BuildContext context) {
@@ -26,6 +25,7 @@ class _HomeScreenState extends State<HomeScreen> {
       body: Center(
         child: Column(
           children: [
+            Text(answers[0]),
             SizedBox(width: 300, height: 200, child: Image.asset("images\\LSU_Athletics_logo.svg.png", fit: BoxFit.scaleDown,)),
             SizedBox(width: 300, height: 200, child: Image.asset("images\\lsu-patrick-taylor-hall-exterior-architecture.jpg", fit: BoxFit.scaleDown,)),
             SizedBox(width: 300, height: 200, child: 
@@ -57,7 +57,49 @@ class Q1Screen extends StatelessWidget{
         child: Column(
           children: [
             Text("Q1 Screen"),
+            Text("You answered: "),
+            Text(answers[0]),
             ElevatedButton(onPressed: (){answers[0] = "A"; Navigator.pop(context);}, child: Text("Answer 1"))
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Q2Screen extends StatelessWidget{
+  
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 95, 8, 62),
+      body: Center(
+        child: Column(
+          children: [
+            Text("Q2 Screen"),
+            Text("You answered: "),
+            Text(answers[0]),
+            ElevatedButton(onPressed: (){answers[1] = "A"; Navigator.pop(context);}, child: Text("Answer 1"))
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Q3Screen extends StatelessWidget{
+  
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 95, 8, 62),
+      body: Center(
+        child: Column(
+          children: [
+            Text("Q1 Screen"),
+            Text("You answered: "),
+            Text(answers[0]),
+            ElevatedButton(onPressed: (){answers[2] = "A"; Navigator.pop(context);}, child: Text("Answer 1"))
           ],
         ),
       ),
